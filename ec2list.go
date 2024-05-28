023fc6ccc636630c5016e3f0f71e2a1fefb6a7d9
Erik <sertvitas@gmail.com>
2024-05-28 08:02:48 -0400

    Convert from python to go for the version checkers.

---
+++ ec2list.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a partial cluster name as an argument.")
+	}
+
+	partialClusterName := os.Args[1]
+
+	// Load the Shared AWS Configuration (from ~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Create an Amazon ECS client
+	ecsClient := ecs.NewFromConfig(cfg)
+
+	// List clusters
+	clustersOutput, err := ecsClient.ListClusters(context.TODO(), &ecs.ListClustersInput{})
+	if err != nil {
+		log.Fatalf("failed to list clusters, %v", err)
+	}
+
+	// Iterate over each cluster
+	for _, clusterArn := range clustersOutput.ClusterArns {
+		clusterName := extractName(clusterArn)
+
+		if strings.Contains(clusterName, partialClusterName) {
+			// List tasks with desired status RUNNING
+			tasksOutput, err := ecsClient.ListTasks(context.TODO(), &ecs.ListTasksInput{
+				Cluster:       &clusterName,
+				DesiredStatus: "RUNNING",
+			})
+			if err != nil {
+				log.Fatalf("failed to list tasks for cluster %s, %v", clusterName, err)
+			}
+
+			fmt.Printf("Cluster: %s\n", clusterName)
+			for _, taskArn := range tasksOutput.TaskArns {
+				taskID := extractName(taskArn)
+
+				// Describe the task to get the task definition ARN
+				describeTasksOutput, err := ecsClient.DescribeTasks(context.TODO(), &ecs.DescribeTasksInput{
+					Cluster: &clusterName,
+					Tasks:   []string{taskArn},
+				})
+				if err != nil {
+					log.Fatalf("failed to describe task %s, %v", taskID, err)
+				}
+				taskDefinitionArn := describeTasksOutput.Tasks[0].TaskDefinitionArn
+
+				// Describe the task definition to get container definitions
+				taskDefinitionOutput, err := ecsClient.DescribeTaskDefinition(context.TODO(), &ecs.DescribeTaskDefinitionInput{
+					TaskDefinition: taskDefinitionArn,
+				})
+				if err != nil {
+					log.Fatalf("failed to describe task definition %s, %v", *taskDefinitionArn, err)
+				}
+
+				fmt.Printf("\tTask ID: %s\n", taskID)
+				for _, containerDef := range taskDefinitionOutput.TaskDefinition.ContainerDefinitions {
+					containerImage := *containerDef.Image
+					if !strings.Contains(containerImage, "nginx") {
+						fmt.Printf("\t\tContainer Image: %s\n", containerImage)
+					}
+				}
+			}
+		}
+	}
+}
+
+func extractName(arn string) string {
+	parts := strings.Split(arn, "/")
+	return parts[len(parts)-1]
+}
---
+++ ec2table.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a partial cluster name as an argument.")
+	}
+
+	partialClusterName := os.Args[1]
+
+	// Load the Shared AWS Configuration (from ~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Create an Amazon ECS client
+	ecsClient := ecs.NewFromConfig(cfg)
+
+	// List clusters
+	clustersOutput, err := ecsClient.ListClusters(context.TODO(), &ecs.ListClustersInput{})
+	if err != nil {
+		log.Fatalf("failed to list clusters, %v", err)
+	}
+
+	clusterArns := []string{}
+	for _, clusterArn := range clustersOutput.ClusterArns {
+		if strings.Contains(clusterArn, partialClusterName) {
+			clusterArns = append(clusterArns, clusterArn)
+		}
+	}
+
+	if len(clusterArns) == 0 {
+		fmt.Printf("No matching clusters found for filter: %s\n", partialClusterName)
+		return
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Cluster Name", "Task Definition", "Desired Count", "Pending Count", "Running Count", "Container Image"})
+
+	for _, clusterArn := range clusterArns {
+		clusterName := extractName(clusterArn)
+
+		// List services in the cluster
+		servicesOutput, err := ecsClient.ListServices(context.TODO(), &ecs.ListServicesInput{
+			Cluster: &clusterArn,
+		})
+		if err != nil {
+			log.Fatalf("failed to list services for cluster %s, %v", clusterArn, err)
+		}
+
+		if len(servicesOutput.ServiceArns) == 0 {
+			continue
+		}
+
+		// Describe the services to get their details
+		describeServicesOutput, err := ecsClient.DescribeServices(context.TODO(), &ecs.DescribeServicesInput{
+			Cluster:  &clusterArn,
+			Services: servicesOutput.ServiceArns,
+		})
+		if err != nil {
+			log.Fatalf("failed to describe services for cluster %s, %v", clusterArn, err)
+		}
+
+		for _, service := range describeServicesOutput.Services {
+			for _, deployment := range service.Deployments {
+				taskDefinition := deployment.TaskDefinition
+
+				// Describe the task definition to get container definitions
+				taskDefinitionOutput, err := ecsClient.DescribeTaskDefinition(context.TODO(), &ecs.DescribeTaskDefinitionInput{
+					TaskDefinition: taskDefinition,
+				})
+				if err != nil {
+					log.Fatalf("failed to describe task definition %s, %v", *taskDefinition, err)
+				}
+
+				containerImages := ""
+				for _, containerDef := range taskDefinitionOutput.TaskDefinition.ContainerDefinitions {
+					containerImage := extractImageName(*containerDef.Image)
+					if !strings.Contains(containerImage, "nginx") {
+						if containerImages != "" {
+							containerImages += ", "
+						}
+						containerImages += containerImage
+					}
+				}
+
+				t.AppendRow(table.Row{
+					clusterName,
+					extractName(*taskDefinition),
+					deployment.DesiredCount,
+					deployment.PendingCount,
+					deployment.RunningCount,
+					containerImages,
+				})
+			}
+		}
+	}
+
+	t.Render()
+}
+
+func extractName(arn string) string {
+	parts := strings.Split(arn, "/")
+	return parts[len(parts)-1]
+}
+
+func extractImageName(image string) string {
+	parts := strings.Split(image, "/")
+	return parts[len(parts)-1]
+}
